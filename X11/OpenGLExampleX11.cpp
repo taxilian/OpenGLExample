2ac839db2f4d08ebbe9b9ca4fa1b58eb5deb33a8
Richard Bateman <taxilian@gmail.com>
2011-03-05 11:15:28 -0800

    Still not working, added a bunch of stuff

--- X11/OpenGLExampleX11.cpp
+++ X11/OpenGLExampleX11.cpp
@@ -2,6 +2,7 @@
 #include "OpenGLExampleX11.h"
 #include <gtk/gtkgl.h>
 #include <GL/gl.h>
+#include "logging.h"
 
 void OpenGLExampleX11::StaticInitialize()
 {
@@ -36,31 +37,70 @@ bool OpenGLExampleX11::onWindowDetached(FB::DetachedEvent *evt, FB::PluginWindow
 
 void OpenGLExampleX11::renderThread(FB::PluginWindowX11* win)
 {
+    gdk_threads_enter();
     FBLOG_WARN("", "Starting Render Thread");
+
+    GtkWidget* drawing_area;
+    drawing_area = gtk_drawing_area_new();
+    gtk_widget_set_size_request(drawing_area, win->getWindowWidth(), win->getWindowHeight());
+
     GdkGLConfigMode glconf = static_cast<GdkGLConfigMode>(
             GDK_GL_MODE_RGB |
             GDK_GL_MODE_DOUBLE |
-            GDK_GL_MODE_ACCUM |
-            GDK_GL_MODE_ACCUM
+            GDK_GL_MODE_DEPTH
             );
+    FBLOG_WARN("", "Trying to config");
     glConfig = gdk_gl_config_new_by_mode(glconf);
+    FBLOG_WARN("", "Created config: " << glConfig);
+    if (!glConfig) {
+        FBLOG_WARN("", "Double-buffer didn't work. Trying single.");
+        glconf = static_cast<GdkGLConfigMode>(
+                GDK_GL_MODE_RGB | GDK_GL_MODE_DEPTH
+                );
+        glConfig = gdk_gl_config_new_by_mode(glconf);
+        FBLOG_WARN("", "Created config: " << glConfig);
+        if (!glConfig) {
+            FBLOG_WARN("", "Couldn't initialize opengl. No idea why, sorry!");
+            return;
+        } else {
+            FBLOG_INFO("", "Double-buffer opengl");
+        }
+    } else {
+        FBLOG_INFO("", "Double-buffer opengl");
+    }
 
-    gtk_widget_set_gl_capability(win->getWidget(), glConfig, NULL, TRUE, GDK_GL_RGBA_TYPE);
+    FBLOG_WARN("", "Setting GL capability on Widget: " << drawing_area);
+    gtk_widget_set_gl_capability (drawing_area,
+            glConfig,
+            NULL,
+            TRUE,
+            GDK_GL_RGBA_TYPE);
 
-    GdkGLContext *glcontext = gtk_widget_get_gl_context (win->getWidget());
-    GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (win->getWidget());
+    gtk_container_add(win->getGtkContainer(), drawing_area);
+    gtk_widget_show(drawing_area);
+
+    FBLOG_WARN("", "Getting GL Context");
+    GdkGLContext *glcontext = gtk_widget_get_gl_context (drawing_area);
+    FBLOG_WARN("", "Got GL Context: " << glcontext << " Now getting Drawable:");
+    GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (drawing_area);
+    FBLOG_WARN("", "Got GL Drawable: " << gldrawable);
 
     // Attach our OpenGL context to the widget
+    FBLOG_WARN("", "Setting current context to widget");
     gdk_gl_drawable_make_current(gldrawable, glcontext);
+    gdk_threads_leave();
     try {
         while (!boost::this_thread::interruption_requested()) {
+            gdk_threads_enter();
             render();
-            GdkGLDrawable *gld = gtk_widget_get_gl_drawable (win->getWidget());
+            GdkGLDrawable *gld = gtk_widget_get_gl_drawable (drawing_area);
+            FBLOG_INFO("", "Got drawable: " << gld);
             if (gdk_gl_drawable_is_double_buffered (gld)) {
                 gdk_gl_drawable_swap_buffers (gld);
             } else {
                 glFlush();
             }
+            gdk_threads_leave();
             boost::this_thread::sleep(boost::posix_time::milliseconds(20));
         }
     } catch (const boost::thread_interrupted&) {
@@ -70,7 +110,7 @@ void OpenGLExampleX11::renderThread(FB::PluginWindowX11* win)
         g_object_unref(G_OBJECT(glConfig));
         glConfig = NULL;
     }
-    GdkGLContext *glContext = gtk_widget_get_gl_context(win->getWidget());
+    GdkGLContext *glContext = gtk_widget_get_gl_context(drawing_area);
     if (glContext) {
         g_object_unref(G_OBJECT(glContext));
         glContext = NULL;
